874cd1b7a9e84387e1721ce3987e79afb9c5aa5f
Eduard Ciofu <cfedward7@gmail.com>
2025-10-14 23:05:39 +0200

    player controller

---
+++ Source/Droid/Private/Player/D_PlayerController.cpp
@@ -0,0 +1,66 @@
+﻿// Copyright Eduard Ciofu
+
+#include "Player/D_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "GameFramework/Character.h"
+
+void AD_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (!IsValid(InputSubsystem)) return;
+	
+	for (const UInputMappingContext* Context : InputMappingContexts)
+	{
+		InputSubsystem->AddMappingContext(Context, 0);
+	}
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (!IsValid(EnhancedInputComponent)) return;
+
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ThisClass::StopJumping);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+}
+
+void AD_PlayerController::Jump()
+{
+	if (!IsValid(GetCharacter())) return;
+
+	GetCharacter()->Jump();
+}
+
+void AD_PlayerController::StopJumping()
+{
+	if (!IsValid(GetCharacter())) return;
+
+	GetCharacter()->StopJumping();
+}
+
+void AD_PlayerController::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddYawInput(LookAxisVector.X);
+	AddPitchInput(LookAxisVector.Y);
+}
+
+void AD_PlayerController::Move(const FInputActionValue& Value)
+{
+	if (!IsValid(GetPawn())) return;
+
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	// Find which way is forward
+	const FRotator YawRotation(0.f, GetControlRotation().Yaw, 0.f);
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	GetPawn()->AddMovementInput(ForwardDirection, MovementVector.Y);
+	GetPawn()->AddMovementInput(RightDirection, MovementVector.X);
+}
---
+++ Source/Droid/Public/Player/D_PlayerController.h
@@ -0,0 +1,38 @@
+﻿// Copyright Eduard Ciofu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "D_PlayerController.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class DROID_API AD_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void SetupInputComponent() override;
+
+private:
+
+	void Jump();
+	void StopJumping();
+	void Look(const FInputActionValue& Value);
+	void Move(const FInputActionValue& Value);
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Droid|Input")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Droid|Input")
+	TObjectPtr<UInputAction> JumpAction;
+	UPROPERTY(EditDefaultsOnly, Category = "Droid|Input")
+	TObjectPtr<UInputAction> LookAction;
+	UPROPERTY(EditDefaultsOnly, Category = "Droid|Input")
+	TObjectPtr<UInputAction> MoveAction;
+};
