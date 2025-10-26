0bcd335188ca62c6be6a4be0caac9e2a4b72e2ed
Eduard Ciofu <cfedward7@gmail.com>
2025-10-26 16:03:49 +0100

    enemy class

---
+++ Source/Droid/Private/Characters/D_EnemyCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright Eduard Ciofu
+
+
+#include "Characters/D_EnemyCharacter.h"
+#include "AbilitySystemComponent.h"
+
+AD_EnemyCharacter::AD_EnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+void AD_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!IsValid(GetAbilitySystemComponent())) return;
+
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	
+	if (!HasAuthority()) return;
+
+	GiveStartupAbilities();
+}
+
+UAbilitySystemComponent* AD_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
---
+++ Source/Droid/Public/Characters/D_EnemyCharacter.h
@@ -0,0 +1,30 @@
+﻿// Copyright Eduard Ciofu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "D_BaseCharacter.h"
+#include "D_EnemyCharacter.generated.h"
+
+UCLASS()
+class DROID_API AD_EnemyCharacter : public AD_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	AD_EnemyCharacter();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	/** AbilitySystem Interface */
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	/** end AbilitySystem Interface */
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
